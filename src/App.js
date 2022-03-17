867f2dba1f05cfc59cab8f59b32c468c4de3ae8d
coderrakibul <coder.rakibul@gmail.com>
2022-03-17 12:19:54 +0600

    country loaded

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <loadCountries> </loadCountries>
+      <LoadCountries> </LoadCountries>
     </div>
   );
 }
 
-function loadCountries(){
+function LoadCountries(){
+  const [countries, setCountries] = useState([])
+
+  useEffect( () =>{
+    fetch("https://restcountries.com/v3.1/all")
+    .then(response => response.json())
+    .then(data => setCountries(data))
+  }, [])
+
   return (
     <div>
       <h1>Visiting Every Country In The World</h1>
+      <h3>Available Countries: {countries.length}</h3>
+      {
+        countries.map(country => <Country name={country.name.common} population={country.population}></Country>)
+      }
+    </div>
+  )
+}
+
+function Country(props){
+  return (
+    <div>
+      <h2>Country Name: {props.name}</h2>
+      <h4>Population{props.population}</h4>
     </div>
   )
 }
